231492bac594be22615a8b2c9dd486cc208c6baa
PabloC <pablo.coello@outlook.es>
2020-07-01 11:22:17 +0200

    Added res.txt from optimal design

--- choiceDes.R
+++ choiceDes.R
@@ -1,4 +1,13 @@
 if(!require(choiceDes)){install.packages("choiceDes");library(choiceDes)}
+
+
 levs1 <- c(3,3,3)
-des <- dcm.design(levs1, 3, 3, 20)
+nb = 1 # Número de bloques
+sets = 10 # Numero de choice sets
+alts = 3 # Numero de alternativas en cada choice set
+
+
+des <- dcm.design(levs1, nb, sets, alts,fname='res.txt', 20)
 des
+eff <- dcm.design.effcy(des$effects$design)
+eff
