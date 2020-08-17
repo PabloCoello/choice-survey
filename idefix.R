2e1ed71850379cbd6d9db4be17c3fd8a13576eff
Pablo Coello <pablo.coello@outlook.com>
2020-08-17 17:40:28 +0200

    Update

--- idefix.R
+++ idefix.R
@@ -4,60 +4,61 @@ if(!require(mlogit)){install.packages("mlogit");library(mlogit)}
 if(!require(rsconnect)){install.packages("rsconnect");library(rsconnect)}
 if(!require(parallel)){install.packages("parallel");library(parallel)}
 if(!require(doSNOW)){install.packages("doSNOW");library(doSNOW)}
-
+if(!require(rjson)){install.packages("rjson");library(rjson)}
 
 cores <- detectCores(all.tests = FALSE, logical = TRUE)
 cluster_name <- makeCluster(cores, type = "SOCK")
+memory.limit(9999999999)
 #rsconnect::setAccountInfo(name='ecoiuris',
 #                          token='45F8C61BCA58B3EC9C85948E570C423B',
 #                          secret='6nEgZw5xO1FwGLJXlB5M719M6ZeC8eK7RWmGR0yc')
 #rsconnect::deployApp('path/to/your/app')
 #Parameters
 
-dataDir <-
-  "C:/Users/pablo/OneDrive - Universidade de Santiago de Compostela/surveys"
-setwd(dataDir)
-load(file="adaptive_design_12.RData")
+gen_lvls <- function(conf){
+  lvls <- c()
+  for(i in 1:length(conf[["attributes"]])){
+    lvls <- c(lvls, length(conf[[paste('labels',i,sep='')]]))
+  } 
+  return(lvls)
+}
 
-code <- c("D", "D", "D", "D")
-alternatives <- c("Alternativa A", "Alternativa B", "Alternativa C")
-attributes <- c("Vulnerabilidad económica", 
-                "Vulnerabilidad social",
-                "Vulnerabilidad medioambiental", 
-                "Coste del tratamiento")
-n.sets <-12
-n.alts <-3
-alt.cte <- NULL
+gen_sigma <- function(conf){
+  if(is.null(conf[["sigma"]])){
+    sigma <- diag(length(conf[['mu']]))
+  }else{
+    sigma <- conf[['sigma']]
+  }
+  return(sigma)
+}
 
-mu <- c(0.4, 1, 0.2, 0.4, 0.3, 0.4, 0.5, 0.7, -0.1, -0.2, -0.3, -0.4)
-sigma <- diag(length(mu))
+generate_design <- function(conf){
+  design[['lvls']] <- gen_lvls(conf)
+  design[['sigma']] <- gen_sigma(conf)
 
-labels <- vector(mode = "list", length(attributes))
-labels[[1]] <- c("0€/ha", "1.000€/ha", "2.000€/ha")
-labels[[2]] <- c("Baja", "Media", "Alta")
-labels[[3]] <- c("No significativa", "Baja", "Media", "Alta", "Muy alta")
-labels[[4]] <- c("2.000€/ha", "4.000€/ha", "6.000€/ha", "8.000€/ha", "10.000€/ha")
-b.text <- "Please choose the alternative you prefer"
-i.text <- "Welcome, here are some instructions ... good luck!"
-e.text <- "Thanks for taking the survey"
+  design[['cs']] <- Profiles(lvls = design[['lvls']], coding = conf[['code']])
+  design[['M']] <- MASS::mvrnorm(n = 500, mu = conf[['mu']], Sigma = design[['sigma']])
+  design[['D']] <- Modfed(
+    cand.set = design[['cs']],
+    n.sets = conf[['n.sets_design']],
+    n.alts = conf[['n.alts']],
+    alt.cte = conf[['alt.cte']],
+    par.draws = design[['M']]
+  )
+  print(paste("Error of the model: ", D$error))
+  return(design)
+}
 
-# Design
 
-cs <- Profiles(lvls = c(3, 3, 5, 5), coding = code)
+setwd(system("pwd", intern = T) )
+conf <- fromJSON(file='./conf.json')
+design <- generate_design(conf)
+save(design, 'design_name.RData')
 
-M <- MASS::mvrnorm(n = 500, mu = mu, Sigma = sigma)
-D <- Modfed(
-  cand.set = cs,
-  n.sets = n.sets,
-  n.alts = n.alts,
-  alt.cte = alt.cte,
-  par.draws = M
-)
-D$error
-xdes <- D$design
 
-DD <- Decode(des=xdes,n.alts = n.alts, lvl.names = labels, coding = code)
-DD
+#DD <- Decode(des=xdes,n.alts = n.alts, lvl.names = labels, coding = code)
+#DD
+
 
 # Adaptive design
 truepref <- c(0.3, 0.4, 0.3, 0.5, 0.3, 0.6, 0.7, 0.8, -0.2, -0.3, -0.4, -0.5)
@@ -69,7 +70,7 @@ draws <- ImpsampMNL(n.draws = 100, prior.mean = mu, prior.covar = sigma,
                     des = xdes, n.alts = n.alts, y = y.sim)
 draws
 
-memory.limit(9999999999)
+
 set <- SeqMOD(des=xdes, cand.set = cs, n.alts = n.alts,
               par.draws = draws$sample, prior.covar = sigma,
               weights= draws$weights, parallel = TRUE, reduce=TRUE)
@@ -82,18 +83,18 @@ set
 
 xdes
 
-
+conf[["survey_conf"]]['b.text']
 SurveyApp (
   des = xdes,
-  n.total = 16,
-  alts = alternatives,
-  atts = attributes,
+  n.total = conf[['n.sets_survey']],
+  alts = conf[['alternatives']],
+  atts = conf[['attributes']],
   lvl.names = labels,
-  coding = code,
-  buttons.text = b.text,
-  intro.text = i.text,
-  end.text = e.text,
-  prior.mean = mu,
+  coding = conf[['code']],
+  buttons.text = conf[["survey_conf"]]['b.text'],
+  intro.text = conf[["survey_conf"]]['i.text'],
+  end.text = conf[["survey_conf"]]['e.text'],
+  prior.mean = conf[['mu']],
   prior.covar = sigma,
   cand.set = cs,
   n.draws = 50,
