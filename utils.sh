a159d217e7cde3240df692a1a00eb388de13df93
Pablo Coello <pablo.coello@outlook.com>
2020-08-17 23:36:24 +0200

    Upgraded utils with interface

--- utils.sh
+++ utils.sh
@@ -2,6 +2,19 @@ DM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WO_DIR=/opt/choice-survey
 DK_IMG=choice-survey-dev-env
 
+while :
+	do
+	echo "1. Build image"
+	echo "2. Enter container"
+	echo "3. Execute container"
+	echo "4. Generate survey design"
+	echo "5. Deploy survey"
+	echo "6. Analyse survey data"
+	echo "7. Modify configuration"
+	echo "8. EXIT"
+	echo -n "Choose one option [1 - 8]: "
+	read opcion
+
 function build_dev_image () {
 	docker build -t "${DK_IMG}" "${DM_DIR}"/Docker/
 }
@@ -14,22 +27,47 @@ function execute () {
 	docker run --rm -it -v  "${DM_DIR}":"${WO_DIR}" --network host "${DK_IMG}" /bin/bash "${WO_DIR}"/utils.sh run
 }
 
-function run () {
-    # entrypoint
-    echo "run"
+function generate_design () {
+	Rscript generate_design.R
 }
 
-case $1 in
-	build)
+function deploy_survey () {
+	Rscript deploy_survey.R
+}
+
+function analyse_data () {
+	Rscript analyse_data.R
+}
+
+function modify_conf () {
+	sudo nano ./conf.json
+}
+
+case $opcion in
+	1)
 		build_dev_image
 		;;
-	enter)
+	2)
 		enter_dev_env
 		;;
-	execute_container)
+	3)
 		execute
 		;;
-	run)
-		run $2
+	4)
+		generate_design
+		;;
+	5)
+		deploy_survey
+		;;
+	6)
+		analyse_data
+		;;
+	7)
+		modify_conf
+		;;
+	8)
+		echo "bye";
+		exit 1
 		;;
 esac
+done
