348b1cf14616dc6dbeb9aa69f75a9182cd935eeb
Pablo Coello <pablo.coello@outlook.com>
2020-08-17 18:54:01 +0200

    Fixed conf bug

--- deploy_survey.R
+++ deploy_survey.R
@@ -9,22 +9,23 @@ cluster_name <- makeCluster(cores, type = "SOCK")
 setwd(system("pwd", intern = T) )
 conf <- fromJSON(file='./conf.json')
 
-load(paste('./Designs/', conf[['design_conf']][['design_name']], '.rds', sep=''))
-
+design <- readRDS(paste('./Designs/', conf[['design_conf']][['design_name']], '.rds', sep=''))
 
 SurveyApp (
-  des = design[['D']]$xdes,
-  n.total = conf[['n.sets_survey']],
-  alts = conf[['alternatives']],
-  atts = conf[['attributes']],
+  des = design[['D']]$design,
+  n.total = conf[['survey_conf']][['n.sets_survey']],
+  alts = conf[["design_conf"]][['alternatives']],
+  atts = conf[["design_conf"]][['attributes']],
   lvl.names = design[['labels']],
-  coding = conf[['code']],
+  coding = conf[["design_conf"]][['att_code']],
   buttons.text = conf[["survey_conf"]]['b.text'],
   intro.text = conf[["survey_conf"]]['i.text'],
   end.text = conf[["survey_conf"]]['e.text'],
-  prior.mean = conf[['mu']],
+  prior.mean = conf[["design_conf"]][['mu']],
   prior.covar = design[['sigma']],
   cand.set = design[['cs']],
   n.draws = 50,
-  data.dir = conf[['path_to_storage']]
+  data.dir = conf[['path_to_storage']],
+  parallel = TRUE, 
+  reduce = TRUE
 )
