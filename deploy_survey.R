0735dc675e5426b6cc64319d82e1c24c8c4a2083
PabloC <pablo.coello@outlook.es>
2020-08-18 13:53:01 +0200

    Update.R

--- deploy_survey.R
+++ deploy_survey.R
@@ -3,6 +3,7 @@ if(!require(parallel)){install.packages("parallel");library(parallel)}
 if(!require(doSNOW)){install.packages("doSNOW");library(doSNOW)}
 if(!require(rjson)){install.packages("rjson");library(rjson)}
 
+
 get_answer <- function(conf){
   n.alts <- conf[['design_conf']][['n.alts']]
   cat(paste("Choose alternative [1-", n.alts, "]: ", sep=''))
@@ -18,15 +19,24 @@ get_answer <- function(conf){
   return(toret)
 }
 
+
 set_rownames <- function(set, i){
-    names <- c()
-    for(n in 1:nrow(set$set)){
-      names[n] <- paste('set', i, '.alt', n , sep='')
-    }
-    rownames(set$set) <- names
-    return(set)
+  names <- c()
+  for(n in 1:nrow(set$set)){
+    names[n] <- paste('set', i, '.alt', n , sep='')
+  }
+  rownames(set$set) <- names
+  return(set)
 }
 
+
+format_set_print <- function(des, i, design, conf){
+  set <- des[grep(paste('set', i, '.alt', sep=''),rownames(des)),]
+  dec <- Decode(des = set, n.alts=conf[['design_conf']][['n.alts']], lvl.names=design[['labels']], coding = conf[["design_conf"]][['att_code']])
+  print(
+    dec$design
+  )
+}
 cores <- detectCores(all.tests = FALSE, logical = TRUE)
 cluster_name <- makeCluster(cores, type = "SOCK")
 
@@ -37,25 +47,27 @@ design <- readRDS(paste('./Designs/', conf[['design_conf']][['design_name']], '.
 
 i <- 0
 des <- design[['D']]$design
-resp<-c()
+resp <- c()
 while(i<conf[['survey_conf']][['n.sets_survey']]){
   i <- i + 1
   if(i <= conf[["design_conf"]][['n.sets_design']]){
-    print(des[grep(paste('set', i, '.alt', sep=''),rownames(des)),])
+    format_set_print(des, i, design, conf)
     resp <- c(resp, get_answer(conf))
   }else{
     draws <- ImpsampMNL(n.draws = 100, prior.mean = conf[["design_conf"]][['mu']], prior.covar =design[['sigma']],
                     des = des, n.alts = conf[['design_conf']][['n.alts']], y = resp)
     set <- SeqCEA(des=des, lvls=design[['lvls']], coding = conf[["design_conf"]][['att_code']],n.alts = conf[['design_conf']][['n.alts']],par.draws = draws$sample,prior.covar = design[['sigma']],weights= draws$weights, parallel = TRUE, reduce=TRUE)
     set <- set_rownames(set, i)
-
-    print(set)
-    resp <- c(resp, get_answer(conf))
     des <- rbind(des, set$set)
+    format_set_print(des, i, design, conf)
+
+    resp <- c(resp, get_answer(conf))
   }
 }
 
 result <- cbind(des, resp)
+
+
 write.table(
   result,
   paste(conf[['path_to_storage']], 
