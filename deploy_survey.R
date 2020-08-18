39356c86e7764fa840c2ac634d8cf0d7bb0ae6f6
Pablo Coello <pablo.coello@outlook.com>
2020-08-18 17:30:45 +0200

    Refactor

--- deploy_survey.R
+++ deploy_survey.R
@@ -4,15 +4,15 @@ if(!require(doSNOW)){install.packages("doSNOW");library(doSNOW)}
 if(!require(rjson)){install.packages("rjson");library(rjson)}
 
 
-get_answer <- function(conf){
+get_answer <- function(conf) {
   n.alts <- conf[['design_conf']][['n.alts']]
-  cat(paste("Choose alternative [1-", n.alts, "]: ", sep=''))
-  answer <- readLines("stdin", n=1)
+  cat(paste("Choose alternative [1-", n.alts, "]: ", sep = ''))
+  answer <- readLines("stdin", n = 1)
   toret <- c()
-  for(i in 1:n.alts){
-    if(i != answer){
+  for (i in 1:n.alts) {
+    if (i != answer) {
       toret[i] <- 0
-    }else{
+    } else{
       toret[i] <- 1
     }
   }
@@ -20,64 +20,89 @@ get_answer <- function(conf){
 }
 
 
-set_rownames <- function(set, i){
+set_rownames <- function(set, i) {
   names <- c()
-  for(n in 1:nrow(set$set)){
-    names[n] <- paste('set', i, '.alt', n , sep='')
+  for (n in 1:nrow(set$set)) {
+    names[n] <- paste('set', i, '.alt', n , sep = '')
   }
   rownames(set$set) <- names
   return(set)
 }
 
 
-format_set_print <- function(des, i, design, conf){
-  set <- des[grep(paste('set', i, '.alt', sep=''),rownames(des)),]
-  dec <- Decode(des = set, n.alts=conf[['design_conf']][['n.alts']], lvl.names=design[['labels']], coding = conf[["design_conf"]][['att_code']])
-  print(
-    dec$design
-  )
+format_set_print <- function(des, i, design, conf) {
+  set <- des[grep(paste('set', i, '.alt', sep = ''), rownames(des)), ]
+  dec <-
+    Decode(
+      des = set,
+      n.alts = conf[['design_conf']][['n.alts']],
+      lvl.names = design[['labels']],
+      coding = conf[["design_conf"]][['att_code']]
+    )
+  print(dec$design)
 }
+
+
 cores <- detectCores(all.tests = FALSE, logical = TRUE)
 cluster_name <- makeCluster(cores, type = "SOCK")
 
-setwd(system("pwd", intern = T) )
-conf <- fromJSON(file='./conf.json')
-design <- readRDS(paste('./Designs/', conf[['design_conf']][['design_name']], '.rds', sep=''))
+setwd(system("pwd", intern = T))
+conf <- fromJSON(file = './conf.json')
+design <- readRDS(paste('./Designs/',
+                        conf[['design_conf']][['design_name']],
+                        '.rds',
+                        sep = ''))
 
 
 i <- 0
 des <- design[['D']]$design
 resp <- c()
-while(i<conf[['survey_conf']][['n.sets_survey']]){
+while (i < conf[['survey_conf']][['n.sets_survey']]) {
   i <- i + 1
-  if(i <= conf[["design_conf"]][['n.sets_design']]){
+  if (i <= conf[["design_conf"]][['n.sets_design']]) {
     format_set_print(des, i, design, conf)
     resp <- c(resp, get_answer(conf))
-  }else{
-    draws <- ImpsampMNL(n.draws = 100, prior.mean = conf[["design_conf"]][['mu']], prior.covar =design[['sigma']],
-                    des = des, n.alts = conf[['design_conf']][['n.alts']], y = resp)
-    set <- SeqCEA(des=des, lvls=design[['lvls']], coding = conf[["design_conf"]][['att_code']],n.alts = conf[['design_conf']][['n.alts']],par.draws = draws$sample,prior.covar = design[['sigma']],weights= draws$weights, parallel = TRUE, reduce=TRUE)
+  } else{
+    draws <-
+      ImpsampMNL(
+        n.draws = 100,
+        prior.mean = conf[["design_conf"]][['mu']],
+        prior.covar = design[['sigma']],
+        des = des,
+        n.alts = conf[['design_conf']][['n.alts']],
+        y = resp
+      )
+    set <-
+      SeqCEA(
+        des = des,
+        lvls = design[['lvls']],
+        coding = conf[["design_conf"]][['att_code']],
+        n.alts = conf[['design_conf']][['n.alts']],
+        par.draws = draws$sample,
+        prior.covar = design[['sigma']],
+        weights = draws$weights,
+        parallel = TRUE,
+        reduce = TRUE
+      )
     set <- set_rownames(set, i)
     des <- rbind(des, set$set)
     format_set_print(des, i, design, conf)
-
+    
     resp <- c(resp, get_answer(conf))
   }
 }
 
 result <- cbind(des, resp)
 
-
+setwd(conf[['path_to_storage']])
 write.table(
-  result,
-  paste(conf[['path_to_storage']], 
-        str(as.numeric(Sys.time())),
-        '_num_data.txt'),
+  as.data.frame(result),
+  sprintf("%s_num_data.txt", as.integer(Sys.time())),
   append = FALSE,
-  sep = " ",
-  dec = ".",
+  sep = "\t",
+  quote = FALSE,
   row.names = TRUE,
-  col.names = TRUE
+  col.names = NA
 )
 
 stopCluster(cluster_name)
--- generate_design.R
+++ generate_design.R
@@ -4,39 +4,41 @@ if(!require(doSNOW)){install.packages("doSNOW");library(doSNOW)}
 if(!require(rjson)){install.packages("rjson");library(rjson)}
 
 
-gen_lvls <- function(conf){
+gen_lvls <- function(conf) {
   lvls <- c()
-  for(i in 1:length(conf[["attributes"]])){
-    lvls <- c(lvls, length(conf[[paste('labels',i,sep='')]]))
-  } 
+  for (i in 1:length(conf[["attributes"]])) {
+    lvls <- c(lvls, length(conf[[paste('labels', i, sep = '')]]))
+  }
   return(lvls)
 }
 
-gen_sigma <- function(conf){
-  if(is.null(conf[["sigma"]])){
+gen_sigma <- function(conf) {
+  if (is.null(conf[["sigma"]])) {
     sigma <- diag(length(conf[['mu']]))
-  }else{
+  } else{
     sigma <- conf[['sigma']]
   }
   return(sigma)
 }
 
-gen_labels <- function(conf){
-    labels <- list()
-    for(i in 1:length(conf[['attributes']])){
-        labels[[i]] <- conf[[paste('labels', i, sep='')]]
-    }
-    return(labels)
+gen_labels <- function(conf) {
+  labels <- list()
+  for (i in 1:length(conf[['attributes']])) {
+    labels[[i]] <- conf[[paste('labels', i, sep = '')]]
+  }
+  return(labels)
 }
 
-generate_design <- function(conf){
+generate_design <- function(conf) {
   design <- list()
   design[['lvls']] <- gen_lvls(conf)
   design[['sigma']] <- gen_sigma(conf)
   design[['labels']] <- gen_labels(conf)
-
-  design[['cs']] <- Profiles(lvls = design[['lvls']], coding = conf[['att_code']])
-  design[['M']] <- MASS::mvrnorm(n = 500, mu = conf[['mu']], Sigma = design[['sigma']])
+  
+  design[['cs']] <-
+    Profiles(lvls = design[['lvls']], coding = conf[['att_code']])
+  design[['M']] <-
+    MASS::mvrnorm(n = 500, mu = conf[['mu']], Sigma = design[['sigma']])
   design[['D']] <- Modfed(
     cand.set = design[['cs']],
     n.sets = conf[['n.sets_design']],
@@ -51,7 +53,10 @@ cores <- detectCores(all.tests = FALSE, logical = TRUE)
 cluster_name <- makeCluster(cores, type = "SOCK")
 
 setwd(system("pwd", intern = T))
-conf <- fromJSON(file='./conf.json')
-design <- generate_design(conf=conf[["design_conf"]])
-saveRDS(design, paste('./Designs/', conf[["design_conf"]][['design_name']], '.rds', sep=''))
+conf <- fromJSON(file = './conf.json')
+design <- generate_design(conf = conf[["design_conf"]])
+saveRDS(design, paste('./Designs/',
+                      conf[["design_conf"]][['design_name']],
+                      '.rds',
+                      sep = ''))
 stopCluster(cluster_name)
