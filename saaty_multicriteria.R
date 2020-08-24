5c91f8d407f4dcebecc4ec52f14b594ab974d046
Pablo Coello <pablo.coellopulido@outlook.es>
2020-08-24 13:15:12 +0200

    Added path_to_file

---
+++ saaty_multicriteria.R
@@ -0,0 +1,30 @@
+library(readxl)
+
+format_multi_df <- function(df){
+  criterios <- df[,grep(c('¿Qué criterio'), colnames(df))]
+  grados <- df[,grep(c('Grado de importancia'), colnames(df))]
+  for(col in 1:ncol(criterios)){
+    colnames(criterios)[col] <- substr(colnames(criterios)[col], start=1, stop = 1)
+    colnames(grados)[col] <- substr(colnames(grados)[col], start=1, stop = 1)
+    #if(is.na(as.numeric(colnames(df[,col])))){
+    #  criterios[,col]<-NULL
+    #  grados[,col]<-NULL
+    #}
+  }
+
+  return(list(criterios, grados))
+}
+
+extract_weights <- function(list){
+  return()
+}
+
+
+setwd(system("pwd", intern = T))
+conf <- fromJSON(file = './conf/conf.json')
+multi_conf <- fromJSON(file = './conf/multicriteria_conf.json')
+
+
+df <- read_excel(multi_conf[['path_to_file']])
+list <- format_multi_df(df)
+grados
