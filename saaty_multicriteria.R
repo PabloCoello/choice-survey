f64625548345a0ae8c55da1105f8f9be84067a30
Pablo Coello <pablo.coellopulido@outlook.es>
2020-08-24 13:28:16 +0200

    Fixded na format in format_multi_df()

--- saaty_multicriteria.R
+++ saaty_multicriteria.R
@@ -6,12 +6,15 @@ format_multi_df <- function(df){
   for(col in 1:ncol(criterios)){
     colnames(criterios)[col] <- substr(colnames(criterios)[col], start=1, stop = 1)
     colnames(grados)[col] <- substr(colnames(grados)[col], start=1, stop = 1)
-    #if(is.na(as.numeric(colnames(df[,col])))){
-    #  criterios[,col]<-NULL
-    #  grados[,col]<-NULL
-    #}
   }
-
+  
+  criterios.na <- complete.cases(criterios)
+  criterios <- criterios[criterios.na,]
+  grados <- grados[criterios.na,]
+  
+  grados.na <- complete.cases(grados)
+  criterios <- criterios[grados.na,]
+  grados <- grados[grados.na,]
   return(list(criterios, grados))
 }
 
@@ -27,4 +30,3 @@ multi_conf <- fromJSON(file = './conf/multicriteria_conf.json')
 
 df <- read_excel(multi_conf[['path_to_file']])
 list <- format_multi_df(df)
-grados
