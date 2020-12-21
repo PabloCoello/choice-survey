c9fe9c163a7697b28043a4818577d7f7dc54b61c
PabloCoello <pablo.coellopulido@outlook.es>
2020-12-21 15:24:34 +0100

    Update

--- analyse_data.R
+++ analyse_data.R
@@ -49,26 +49,80 @@ format_data <- function(data, conf){
   return(data)
 }
 
+idx_format_data <- function(data, conf){
+  data$resp <- as.logical(data$resp)
+  names(data)[1]<- 'id'
+
+  nalt <- conf[['design_conf']][['n.alts']]
+  nvar <- length(conf[['design_conf']][['mu']])
+  variables <- names(data)[4:(4+nvar-1)]
+
+  toret <- data.frame()
+  alt <- 0
+  row <- 1
+  resp <- c()
+  for(i in 1:nrow(data)){
+    alt <- alt + 1
+    for(variable in variables){
+      toret[row, paste(variable, alt, sep='_')] <- data[i, variable]
+    }
+    resp <- c(resp, data[i, 'resp'])
+    if(alt == nalt){
+      alt <- 0
+      toret[row, 'Choice'] <- which(resp)
+      toret[row, 'id'] <- data[i, 'id']
+      row <- row + 1
+      resp <- c()
+    }
+  }
+  
+  toret$chid <- 1:nrow(toret)
+
+  toret <- dfidx(
+    toret,
+    idx = list(c("chid", "id")),
+    choice = "Choice",
+    varying = 1:24,
+    sep = "_"
+  )
+  return(toret)
+}
+
 
 # Set conf and environment:
 setwd(system("pwd", intern = T))
 conf <- fromJSON(file = './conf/conf.json')
 conf <- set_nochoice(conf)
 data <- LoadData(data.dir = conf[["path_to_storage"]], type = "num")
-
+data <- idx_format_data(data, conf)
 # Get data:
 formula <- gen_formula(data)
 data <- format_data(data, conf)
 
 # Perform estimation:
 est <-
-  mlogit(formula,
-          data = data,
-          method = "nr",
-          rpar = "n")
+  mlogit(
+    Choice ~ alt4.cte + Var1 + Var2 + Var32 + Var33 + Var4 | 0,
+    toret,
+    rpar = c(
+      Var1 = 'n',
+      Var2 = 'n',
+      Var32 = 'u',
+      Var33 = 'u',
+      Var4 = 'n'
+    ),
+    R = 100,
+    halton = NA,
+    panel = TRUE
+  )
+summary(choi.mxl)
 
-# Show results:
-summary(est)
 
 
+xchoi.up<- update(choi.mxl, correlation = TRUE)
+summary(choi.up)
 
+cor.mlogit(choi.up)
+lrtest(choi.mxl, choi.up)
+waldtest(choi.up, correlation = FALSE)
+scoretest(choi.mxl, correlation = TRUE)
